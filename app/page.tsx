0ef278a38a92459fd2aa71afa21df08764b350e4
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2026-02-27 10:01:22 +0200

    gamba

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function App (){
   console.log(process.env.PORT)
   return(
     <div className="bg-white w-full h-800">
-      <h1 className="text-green-500">My name is Ranthia</h1>
+      <h1 className="text-green-500">My name is GAMBA</h1>
     </div>
   )
 }
